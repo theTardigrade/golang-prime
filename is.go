9197dcd977fd5ae5ce2c058965e1e168be8f3252
James Smith <theTardigrade@users.noreply.github.com>
2023-02-14 20:41:48 +0000

    Only use big integers if necessary within Is function

--- is.go
+++ is.go
@@ -1,6 +1,7 @@
 package prime
 
 import (
+	"math"
 	"math/big"
 )
 
@@ -18,25 +19,37 @@ func Is(n int64) bool {
 		return false
 	}
 
-	bigN := big.NewInt(n)
-	bigI := big.NewInt(5)
-	bigZero := big.NewInt(0)
-	bigTwo := big.NewInt(2)
-	bigSix := big.NewInt(6)
-	bigTemp := new(big.Int)
+	if n == math.MaxInt64 {
+		return true
+	}
 
-	for bigTemp.Mul(bigI, bigI).Cmp(bigN) <= 0 {
-		if bigTemp.Mod(bigN, bigI) == bigZero {
-			return false
+	if n < int64(math.Sqrt(math.MaxInt64)) {
+		for i := int64(5); i*i <= n; i += 6 {
+			if n%i == 0 || n%(i+2) == 0 {
+				return false
+			}
 		}
+	} else {
+		bigN := big.NewInt(n)
+		bigI := big.NewInt(5)
+		bigZero := big.NewInt(0)
+		bigTwo := big.NewInt(2)
+		bigSix := big.NewInt(6)
+		bigTemp := new(big.Int)
 
-		bigTemp.Add(bigI, bigTwo)
+		for bigTemp.Mul(bigI, bigI).Cmp(bigN) <= 0 {
+			if bigTemp.Mod(bigN, bigI) == bigZero {
+				return false
+			}
 
-		if bigTemp.Mod(bigN, bigTemp) == bigZero {
-			return false
+			bigTemp.Add(bigI, bigTwo)
+
+			if bigTemp.Mod(bigN, bigTemp) == bigZero {
+				return false
+			}
+
+			bigI.Add(bigI, bigSix)
 		}
-
-		bigI.Add(bigI, bigSix)
 	}
 
 	return true
