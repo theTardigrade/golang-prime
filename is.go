ae38c2ec6f2adea72ffd4c76b36a0126a445e6ab
James Smith <theTardigrade@users.noreply.github.com>
2023-02-14 20:26:01 +0000

    Use big integers in Is method

--- is.go
+++ is.go
@@ -1,5 +1,9 @@
 package prime
 
+import (
+	"math/big"
+)
+
 // Is determines whether a given number is prime.
 func Is(n int64) bool {
 	if n <= 1 {
@@ -14,10 +18,25 @@ func Is(n int64) bool {
 		return false
 	}
 
-	for i := int64(5); i*i <= n; i += 6 {
-		if n%i == 0 || n%(i+2) == 0 {
+	bigN := big.NewInt(n)
+	bigI := big.NewInt(5)
+	bigZero := big.NewInt(0)
+	bigTwo := big.NewInt(2)
+	bigSix := big.NewInt(6)
+	bigTemp := new(big.Int)
+
+	for bigTemp.Mul(bigI, bigI).Cmp(bigN) <= 0 {
+		if bigTemp.Mod(bigN, bigI) == bigZero {
 			return false
 		}
+
+		bigTemp.Add(bigI, bigTwo)
+
+		if bigTemp.Mod(bigN, bigTemp) == bigZero {
+			return false
+		}
+
+		bigI.Add(bigI, bigSix)
 	}
 
 	return true
