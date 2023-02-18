5eaca93b86e4288d3018d4ad1337c2532ccfaa7c
James Smith <theTardigrade@users.noreply.github.com>
2023-02-18 16:23:51 +0000

    Create BigNext and BigPrev functions

--- next.go
+++ next.go
@@ -1,8 +1,11 @@
 package prime
 
-import "math"
+import (
+	"math"
+	"math/big"
+)
 
-// Next returns the next prime number after a given number.
+// Next returns the next prime number after a given 64-bit number.
 // It also returns a boolean value that is set to false if no
 // prime number exists after the given number &mdash;
 // not because there is no such number in the mathematical set of integers,
@@ -26,6 +29,26 @@ func Next(n int64) (prime int64, exists bool) {
 
 		if Is(prime) {
 			exists = true
+			break
+		}
+	}
+
+	return
+}
+
+// Next returns the next prime number after a given number of arbitrary size.
+func BigNext(bigN *big.Int) (bigPrime *big.Int) {
+	if bigN.Cmp(bigOne) <= 0 {
+		bigPrime = big.NewInt(2)
+		return
+	}
+
+	bigPrime = (new(big.Int)).Set(bigN)
+
+	for {
+		bigPrime.Add(bigPrime, bigOne)
+
+		if BigIs(bigPrime) {
 			break
 		}
 	}
--- prev.go
+++ prev.go
@@ -1,6 +1,8 @@
 package prime
 
-// Prev returns the previous prime number before a given number.
+import "math/big"
+
+// Prev returns the previous prime number before a given 64-bit number.
 // It also returns a boolean value that is set to false if no
 // prime number exists before the given number.
 func Prev(n int64) (prime int64, exists bool) {
@@ -15,6 +17,29 @@ func Prev(n int64) (prime int64, exists bool) {
 		prime--
 
 		if Is(prime) {
+			exists = true
+			break
+		}
+	}
+
+	return
+}
+
+// BigPrev returns the previous prime number before a given number of arbitrary size.
+// It also returns a boolean value that is set to false if no
+// prime number exists before the given number.
+func BigPrev(bigN *big.Int) (bigPrime *big.Int, exists bool) {
+	bigPrime = (new(big.Int)).Set(bigN)
+
+	for {
+		if bigPrime.Cmp(bigOne) <= 0 {
+			bigPrime = nil
+			break
+		}
+
+		bigPrime.Sub(bigPrime, bigOne)
+
+		if BigIs(bigPrime) {
 			exists = true
 			break
 		}
